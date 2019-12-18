883d3b152cb754807dd9a3362e97e79c62bd5568
Vixo341 <malpa341@gmail.com>
2019-12-18 15:10:58 +0100

    add database SQLITE and create first table Post

---
+++ Backend/src/Migrations/Version20191218140803.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191218140803 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('ALTER TABLE post ADD COLUMN slug VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TEMPORARY TABLE __temp__post AS SELECT id, title, published, content, author FROM post');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('CREATE TABLE post (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, published DATETIME NOT NULL, content CLOB NOT NULL, author VARCHAR(255) NOT NULL)');
+        $this->addSql('INSERT INTO post (id, title, published, content, author) SELECT id, title, published, content, author FROM __temp__post');
+        $this->addSql('DROP TABLE __temp__post');
+    }
+}
